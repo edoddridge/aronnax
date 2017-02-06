a9d0b113d5a881eed070c362ea46784eb454eaee
Ed Doddridge <edoddridge@gmail.com>
2017-02-06 11:27:22 -0500

    python module

---
+++ utils/MIMutils/core.py
@@ -0,0 +1,28 @@
+"""!
+The main file with the class definitions
+
+Core
+==============
+
+This file contains all of the classes for the module.
+"""
+
+import numpy as np
+
+class Grid():
+    """!Make a grid object containing all of the axes."""
+
+    def __init__(self,nx,ny,dx,dy,x0=0,y0=0):
+        """!Instantiate a grid object for MIM."""
+
+        # axes for vorticity points
+        self.xp1 = np.linspace(y0,nx*dx+x0,nx+1)
+        self.yp1 = np.linspace(y0,ny*dy+y0,ny+1)
+
+        # Axes for tracer points.
+        self.x = (self.xp1[1:] + self.xp1[:-1])/2.
+        self.y = (self.yp1[1:] + self.yp1[:-1])/2.
+
+
+def load_field(filename):
+    """!Load a variable file."""
