72a8ea9065a0ac9a63cadf0565e8edafe10474c7
Alexey Radul <axch@mit.edu>
2017-02-26 13:17:52 -0500

    Abstract the protocol for running a general experiment.  The only variation is the resolution and the input generator.

--- test/output_preservation_test.py
+++ test/output_preservation_test.py
@@ -36,7 +36,7 @@ def compile_mim(nx, ny, layers):
         "> MIM.f90", shell=True)
     sub.check_call(["gfortran", "-Ofast", "MIM.f90", "-o", "MIM"])
 
-def write_input_f_plane(nx, ny):
+def write_input_f_plane(nx, ny, _layers):
     with fortran_file('fu.bin', 'w') as f:
         f.write_record(np.ones((nx, ny+1), dtype=np.float64)*10e-4)
     with fortran_file('fv.bin', 'w') as f:
@@ -51,15 +51,17 @@ def write_input_f_plane(nx, ny):
     with fortran_file('initH.bin', 'w') as f:
         f.write_record(np.ones((nx, ny), dtype=np.float64)*10e-4)
 
+def run_experiment(write_input, nx, ny, layers):
+    sub.check_call(["rm", "-rf", "input/"])
+    sub.check_call(["rm", "-rf", "output/"])
+    sub.check_call(["mkdir", "-p", "output/"])
+    with working_directory("input"):
+        write_input(nx, ny, layers)
+    compile_mim(nx, ny, layers)
+    sub.check_call(["MIM"])
+
+
 def test_f_plane_red():
-    nx = 10
-    ny = 10
     with working_directory(p.join(self_path, "f_plane_red")):
-        sub.check_call(["rm", "-rf", "input/"])
-        sub.check_call(["rm", "-rf", "output/"])
-        sub.check_call(["mkdir", "-p", "output/"])
-        with working_directory("input"):
-            write_input_f_plane(nx, ny)
-        compile_mim(nx, ny, 1)
-        sub.check_call(["MIM"])
+        run_experiment(write_input_f_plane, 10, 10, 1)
         sub.check_call(["diff", "-ru", "good-output/", "output/"])
