642cac00028152b58b093b6aa30242fc87881a56
Alexey Radul <axch@mit.edu>
2017-03-12 22:14:01 -0400

    Whitespace.

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -7,7 +7,7 @@ self_path = p.dirname(p.abspath(__file__))
 root_path = p.dirname(self_path)
 
 import sys
-sys.path.append(p.join(root_path, 'test') )
+sys.path.append(p.join(root_path, 'test'))
 import output_preservation_test as opt
 
 
