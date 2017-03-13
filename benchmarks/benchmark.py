7c32343998f0ceb207ab844657db5c6683c68327
Alexey Radul <axch@mit.edu>
2017-03-12 21:55:11 -0400

    Space after comma.

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -14,7 +14,7 @@ sys.path.append(p.join(root_path, 'test') )
 import output_preservation_test as opt
 
 
-grid_points = np.array([10,20,40,60,80,100,150,200,300,400,500])
+grid_points = np.array([10, 20, 40, 60, 80, 100, 150, 200, 300, 400, 500])
 
 with opt.working_directory(root_path):
     sub.check_call(["make", "MIM_test"])
@@ -55,13 +55,13 @@ def benchmark_gaussian_bump_red_grav():
                   opt.write_input_beta_plane_bump_red_grav, nx, nx, 1, mim_exec)
         
         plt.figure()
-        plt.plot(grid_points,run_time_O1,'-*',label='MIM run time -O1')
-        plt.plot(grid_points,run_time_Ofast,'-*',label='MIM run time -Ofast')
-        plt.plot(grid_points,(run_time_O1[-7]/(grid_points[-7]**2))*grid_points**2,'-*',label='O(nx*nx)')
+        plt.plot(grid_points, run_time_O1, '-*', label='MIM run time -O1')
+        plt.plot(grid_points, run_time_Ofast, '-*', label='MIM run time -Ofast')
+        plt.plot(grid_points, (run_time_O1[-7]/(grid_points[-7]**2))*grid_points**2, '-*', label='O(nx*nx)')
         plt.legend()
         plt.xlabel('nx')
         plt.ylabel('run time (s)')
-        plt.savefig('beta_plane_bump_red_grav scaling.png',dpi=150)
+        plt.savefig('beta_plane_bump_red_grav scaling.png', dpi=150)
 
 
 def benchmark_gaussian_bump():
@@ -80,19 +80,19 @@ def benchmark_gaussian_bump():
                   opt.write_input_beta_plane_bump, nx, nx, 2, mim_exec)
         
         plt.figure()
-        plt.plot(grid_points[:7],run_time_O1[:7],'-*',label='MIM run time -O1')
-        plt.plot(grid_points[:7],run_time_Ofast[:7],'-*',
+        plt.plot(grid_points[:7], run_time_O1[:7], '-*', label='MIM run time -O1')
+        plt.plot(grid_points[:7], run_time_Ofast[:7], '-*',
             label='MIM run time -Ofast')
         plt.plot(grid_points[:7],
             (run_time_O1[-6]/(grid_points[-6]**2))*grid_points[:7]**2,
-            '-*',label='O(nx**2)')
+            '-*', label='O(nx**2)')
         plt.plot(grid_points[:7],
             (run_time_O1[-6]/(grid_points[-6]**3))*grid_points[:7]**3,
-            '-*',label='O(nx**3)')
+            '-*', label='O(nx**3)')
         plt.legend()
         plt.xlabel('nx')
         plt.ylabel('run time (s)')
-        plt.savefig('beta_plane_bump scaling.png',dpi=150)
+        plt.savefig('beta_plane_bump scaling.png', dpi=150)
 
 if __name__ == '__main__':
     benchmark_gaussian_bump_red_grav()
