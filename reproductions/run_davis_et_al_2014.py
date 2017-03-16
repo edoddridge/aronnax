6bf9ea8ee5499bb0a8f3b16708eaf3f7884c9e3b
Ed Doddridge <edoddridge@gmail.com>
2017-03-16 17:41:40 -0400

    module imports

---
+++ reproductions/run_davis_et_al_2014.py
@@ -0,0 +1,19 @@
+import os.path as p
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+self_path = p.dirname(p.abspath(__file__))
+root_path = p.dirname(self_path)
+
+import sys
+sys.path.append(p.join(root_path, 'test'))
+sys.path.append(p.join(root_path, 'reproductions/Davis_et_al_2014'))
+
+import output_preservation_test as opt
+
+import subprocess as sub
+
+import MIMutils as mim
+
+import time
