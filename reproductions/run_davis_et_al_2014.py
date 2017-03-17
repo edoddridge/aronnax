e9ee0e432181bcabfa46c09df6fa5277ff819267
Ed Doddridge <edoddridge@gmail.com>
2017-03-17 09:21:17 -0400

    plot inputs

--- reproductions/run_davis_et_al_2014.py
+++ reproductions/run_davis_et_al_2014.py
@@ -78,6 +78,11 @@ def write_input_davis_et_al_2014(nx, ny, layers, nTimeSteps,dt):
         initH = 400.*np.ones(X.shape)
         f.write_record(initH.astype(np.float64))
 
+        plt.pcolormesh(X,Y,initH)
+        plt.colorbar()
+        plt.savefig('initH.png',dpi=150)
+        plt.close()
+
 
 def write_davis_wetmask(grid):
     """Write the wet mask file for a recreation of Davis et al. (2014)."""
@@ -99,44 +104,77 @@ def write_davis_wetmask(grid):
         wetmask[ :,-1] = 0
         f.write_record(wetmask.astype(np.float64))
 
+        plt.pcolormesh(X,Y,wetmask)
+        plt.colorbar()
+        plt.savefig('wetmask.png',dpi=150)
+        plt.close()
+
 def write_davis_wind(grid):
     """produce the wind forcing files for a recreation of Davis et al. (2014)."""
     
     L = 750e3
         
     with opt.fortran_file('tau_x.bin', 'w') as f:
-        L = 750e3
         X,Y = np.meshgrid(grid.xp1,grid.y)
-        r = np.sqrt((Y-1965e3)**2 + (X-765e3)**2)
-        theta = np.arctan2(Y-1965e3,X-765e3)
+        r = np.sqrt((Y-1950e3)**2 + (X-750e3)**2)
+        theta = np.arctan2(Y-1950e3,X-750e3)
 
-        tau_x = np.sin(theta)*(r/4. + np.sin(np.pi*r/(2.*L))/4. + np.cos(np.pi*r/(2.*L))/(r*8.))
-        tau_x[Y<1200e3] = (tau_x[82,50]*(1965e3-Y[Y<1200e3]))/(r[Y<1200e3])**2
-        tau_x = tau_x/np.max(np.absolute(tau_x[:,:]))
+        norm = (L*(2+np.pi)/2.)/(4*np.pi);
+        
+        tau_x = (L/(2*np.pi))*np.sin(np.pi*r/L)+(r/4)+(L/(4*np.pi))*(np.cos(np.pi*r/L)-1);
+        tau_x = -tau_x*np.sin(theta)/norm;
 
-
+        tau_x[Y<1000e3] = 0
+        #-2*L*((np.pi-2)/(np.pi+2))*(-Y[Y<1200e3]/r[Y<1200e3]**2)
+        #tau_x = np.sin(theta) * (r**2 * np.pi / (8. * L) + r * np.sin(r * np.pi / L)/4. + L * np.cos(r * np.pi / L)/(4. * r**2 * np.pi))
+        #np.sin(theta)*(r/(4.*L) + np.sin(np.pi*r/(2.*L))/4. + np.cos(np.pi*r/(2.*L))/(r*8.))
+        #tau_x[Y<1200e3] = (tau_x[82,50]*(1950e3-Y[Y<1200e3]))/(r[Y<1200e3])**2
+        #tau_x = tau_x/np.max(np.absolute(tau_x[:,:]))
         f.write_record(tau_x.astype(np.float64))
+
+        plt.pcolormesh(X,Y,tau_x,cmap='RdBu')
+        plt.colorbar()
+        plt.savefig('tau_x.png',bbox_inches='tight')
+        plt.close()
+
 
     with opt.fortran_file('tau_y.bin', 'w') as f:
         X,Y = np.meshgrid(grid.x,grid.yp1)
-        r = np.sqrt((Y-1965e3)**2 + (X-765e3)**2)
-        theta = np.arctan2(Y-1965e3,X-765e3)
+        r = np.sqrt((Y-1950e3)**2 + (X-750e3)**2)
+        theta = np.arctan2(Y-1950e3,X-750e3)
 
-        tau_y = -np.cos(theta)*(np.pi*r/(8.*L) + 
-            np.sin(np.pi*r/(2.*L))/4. + np.cos(np.pi*r/(2.*L))/(r*8.))
-        tau_y[Y<1250e3] = -tau_x[82,50]*(765e3 - X[Y<1250e3])/(r[Y<1250e3])**2
-        tau_y = tau_y/np.max(np.absolute(tau_y[:,-1]))
+        norm = (L*(2+np.pi)/2.)/(4*np.pi);
+        
+        tau_y = (L/(2*np.pi))*np.sin(np.pi*r/L)+(r/4)+(L/(4*np.pi))*(np.cos(np.pi*r/L)-1);
+        tau_y = tau_y*np.cos(theta)/norm;
 
+        tau_y[Y<1000e3] = 0
+
+        # tau_y = -np.cos(theta)*(np.pi*r/(8.*L) + 
+        #     np.sin(np.pi*r/(2.*L))/4. + np.cos(np.pi*r/(2.*L))/(r*8.))
+        # tau_y[Y<1250e3] = -tau_x[82,50]*(750e3 - X[Y<1250e3])/(r[Y<1250e3])**2
+        # tau_y = tau_y/np.max(np.absolute(tau_y[:,-1]))
         f.write_record(tau_y.astype(np.float64))
+
+        plt.pcolormesh(X,Y,tau_y,cmap='RdBu')
+        plt.colorbar()
+        plt.savefig('tau_y.png',bbox_inches='tight')
+        plt.close()
 
 def write_davis_sponge(grid):
     """Produce the sponge file used by Davis et al. (2014)."""
     with opt.fortran_file('sponge_h_timescale.bin', 'w') as f:
         X,Y = np.meshgrid(grid.x,grid.y)
         # start with land everywhere and carve out space for water
-        sponge_h = np.zeros(X.shape, dtype=np.float64)
-        sponge_h[Y<300e3] = 1/(6.*30.*86400.) # six month relaxation time
-        f.write_record(sponge_h)
+        sponge_h_timescale = np.zeros(X.shape, dtype=np.float64)
+        sponge_h_timescale[Y<300e3] = 1/(3.*30.*86400.) # six month relaxation time
+        f.write_record(sponge_h_timescale)
+
+        plt.pcolormesh(X,Y,sponge_h_timescale*86400.*30.)
+        plt.colorbar()
+        plt.savefig('sponge_h_timescale.png',dpi=150)
+        plt.close()
+
 
     with opt.fortran_file('sponge_h.bin', 'w') as f:
         X,Y = np.meshgrid(grid.x,grid.y)
@@ -144,12 +182,20 @@ def write_davis_sponge(grid):
         sponge_h = 400.*np.ones(X.shape, dtype=np.float64)
         f.write_record(sponge_h)
 
+        plt.pcolormesh(X,Y,sponge_h)
+        plt.colorbar()
+        plt.savefig('sponge_h.png',dpi=150)
+        plt.close()
+
 def write_wind_time_series(nTimeSteps,dt):
     with opt.fortran_file('wind_time_series.bin', 'w') as f:
         wind_time_series = 0.02375*np.ones(nTimeSteps,dtype=np.float64)
         time = np.arange(nTimeSteps)*dt
         wind_time_series[(np.mod(time,12.*30.*86400.)>8.*30.*86400.)] = 0.0125
         f.write_record(wind_time_series)
+        plt.plot(time/86400./30/12, wind_time_series)
+        plt.savefig('wind_time_series.png')
+        plt.close()
 
 
 
