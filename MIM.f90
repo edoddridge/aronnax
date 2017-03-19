8b90bcd1f5c13063ab7a1029e4ad49ae22f3674e
Alexey Radul <axch@mit.edu>
2017-03-19 01:12:38 -0400

    Pull the height normalization loop out into a subroutine.

--- MIM.f90
+++ MIM.f90
@@ -364,15 +364,8 @@ program MIM
 
     ! Check that the supplied free surface anomaly and layer
     ! thicknesses are consistent
-    h_norming = (freesurfFac*eta+depth)/sum(h,3)
-    do k = 1, layers
-      h(:, :, k) = h(:, :, k) * h_norming
-    end do
+    call enforce_moderate_free_surface(h, eta, depth, freesurfFac, thickness_error, nx, ny, layers)
 
-    if (maxval(abs(h_norming - 1d0)).gt. thickness_error) then
-      print *, 'inconsistency between h and eta (in %):', &
-          maxval(abs(h_norming - 1d0))*100d0
-    end if
   end if
 
   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -1310,6 +1303,34 @@ subroutine update_velocities_for_barotropic_tendency(array, etanew, g_vec, xstep
 
   return
 end subroutine update_velocities_for_barotropic_tendency
+
+! -----------------------------------------------------------------------------
+!> Check that the free surface anomaly and layer thicknesses are consistent
+
+subroutine enforce_moderate_free_surface(h, eta, depth, freesurfFac, thickness_error, nx, ny, layers)
+  implicit none
+
+  double precision, intent(inout) :: h(0:nx+1, 0:ny+1, layers)
+  double precision, intent(in) :: eta(0:nx+1, 0:ny+1)
+  double precision, intent(in) :: depth(0:nx+1, 0:ny+1)
+  double precision, intent(in) :: freesurfFac, thickness_error
+  integer, intent(in) :: nx, ny, layers
+
+  integer k
+  double precision h_norming(0:nx+1, 0:ny+1)
+
+  h_norming = (freesurfFac*eta + depth) / sum(h,3)
+  do k = 1, layers
+    h(:, :, k) = h(:, :, k) * h_norming
+  end do
+
+  if (maxval(abs(h_norming - 1d0)) .gt. thickness_error) then
+    print *, 'inconsistency between h and eta (in %):', &
+        maxval(abs(h_norming - 1d0))*100d0
+  end if
+
+  return
+end subroutine enforce_moderate_free_surface
 
 ! -----------------------------------------------------------------------------
 !> Ensure that layer heights do not fall below the prescribed minimum
