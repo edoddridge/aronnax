27c9efc8989d021bb08e186a606b796a41590779
Alexey Radul <axch@mit.edu>
2017-03-26 07:51:17 -0400

    Move Python sources files to standard layout.
    Now this repository "is" a Python package, rather than "has" a Python
    package in the utils/ directory.
    
    Closes #80.

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(name='MIMutils',
       version='0.1',
-      description='Helper package for MIM',
+      description='An idealized isopycnal model with n layers and variable bathymetry.',
       url='http://github.com/edoddridge/MIM',
       author='Ed Doddridge',
       author_email='blank',
       license='MIT licence',
       packages=['MIMutils'],
+      package_dir={"MIMutils": "src"},
       install_requires=[
           'numpy',],
       zip_safe=False)
