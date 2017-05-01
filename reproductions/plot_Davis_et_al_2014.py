80385fddac174ff0a9a182a54a88a1322b090e20
Ed Doddridge <edoddridge@gmail.com>
2017-05-01 10:56:08 -0400

    Only plot one 2D state map per simulation
    Also, save plots to docs folder for incorporation in documentation, makes plots prettier, and swap from u to v component

--- reproductions/plot_Davis_et_al_2014.py
+++ reproductions/plot_Davis_et_al_2014.py
@@ -33,42 +33,95 @@ def plt_h_cross_section(simulation=None):
     for i in xrange(100,160):
         plt.plot(h_final[:,i,0])
     plt.savefig('{0}figures/h_final.png'.format(simulation))
+    plt.close()
 
 def plt_state(simulation=None):
     h_files = sorted(glob.glob("{0}output/snap.h.*".format(simulation)))
-    u_files = sorted(glob.glob("{0}output/snap.u.*".format(simulation)))
+    v_files = sorted(glob.glob("{0}output/snap.v.*".format(simulation)))
 
     h_max = np.zeros(len(h_files))
 
-    for i in xrange(len(u_files)):
+    for i in xrange(len(v_files)):
         h = aro.interpret_raw_file(h_files[i],102,182,1,)
         h_max[i] = np.max(h)
 
-        u = aro.interpret_raw_file(u_files[i],102,182,1,)
+    # plot the final state of the run
+    i = len(h_files) - 1
+    v = aro.interpret_raw_file(v_files[i],102,182,1,)
 
-        X,Y = np.meshgrid(grid.x,grid.y)
+    X,Y = np.meshgrid(grid.x/1e3,grid.y/1e3)
 
-        CS = plt.contour(X,Y,np.transpose(h[:,:,0]),colors='k')
-        plt.clabel(CS, inline=1, fontsize=10)
-        X,Y = np.meshgrid(grid.xp1,grid.y)
+    CS = plt.contour(X,Y,np.transpose(h[:,:,0]),colors='k')
+    plt.clabel(CS, inline=1, fontsize=10)
+    X,Y = np.meshgrid(grid.x/1e3,grid.yp1/1e3)
 
-        plt.pcolormesh(X,Y,np.transpose(u[:,:,0]),cmap='RdBu_r',
-            vmin = -0.02, vmax = 0.02)
-        plt.colorbar()
+    plt.pcolormesh(X,Y,np.transpose(v[:,:,0])*100.,cmap='RdBu_r'
+        ,vmin = -2, vmax = 2)
+    CB = plt.colorbar()
+    CB.set_label('y component of velocity (cm / s)')
+    plt.xlim(0,1500)
+    plt.axes().set_aspect('equal')
+    plt.xlabel('x coordinate (km)')
+    plt.ylabel('y coordinate (km)')
 
-        plt.savefig('{0}figures/state_{1}.png'.format(simulation,u_files[i][-10:]),dpi=150)
-        plt.close()
-
-    plt.plot(h_max)
-    plt.savefig('{0}figures/h_max.png'.format(simulation),dpi=150)
+    plt.savefig('{0}figures/state_{1}.png'.format(simulation,v_files[i][-10:]),dpi=150,
+        bbox_inches='tight')
+    if simulation == 'control_final_five/':
+        plt.savefig('../../docs/Davis_state_{0}.png'.format(v_files[i][-10:]),dpi=150,
+            bbox_inches='tight')
     plt.close()
 
+    if simulation == 'control_final_five/':
+        plt.plot(np.arange(360.)*5./360.,h_max)
+        plt.xlabel('Time (years)')
+    else:
+        plt.plot(h_max)
+    plt.ylabel('Depth and centre of gyre (m)')
+    plt.savefig('{0}figures/h_max.png'.format(simulation),dpi=150)
+    if simulation == 'control_final_five/':
+        plt.savefig('../../docs/Davis_h_max.png',dpi=150,
+            bbox_inches='tight')
+    plt.close()
+
+def plot_channel_transport(simulation=None):
+    h_files = sorted(glob.glob("{0}output/snap.h.*".format(simulation)))
+    v_files = sorted(glob.glob("{0}output/snap.v.*".format(simulation)))
+
+    transport = np.zeros(len(h_files))
+
+    for i in xrange(len(v_files)):
+        h = aro.interpret_raw_file(h_files[i],102,182,1,)
+        v = aro.interpret_raw_file(v_files[i], 102, 182, 1)
+
+
+        transport[i] = np.sum(h[:,70,0]*(v[:,53,0]+v[:,54,0])/2.)*grid.dx/1e6
+
+
+    if simulation == 'control_final_five/':
+        plt.plot(np.arange(360.)*5./360.,transport)
+        plt.hlines(0,0,5)
+        plt.xlabel('Time (years)')
+    else:
+        plt.plot(transport)
+    plt.ylabel('Transport trhough the channel (Sv)')
+    plt.savefig('{0}figures/transport.png'.format(simulation),dpi=150)
+    if simulation == 'control_final_five/':
+        plt.savefig('../../docs/Davis_channel_transport.png',dpi=150,
+            bbox_inches='tight')
+    plt.close()
 
 
 if __name__ == '__main__':
     os.chdir('Davis_et_al_2014')
     sub.check_call(["rm", "-rf", "control/figures/"])
     sub.check_call(["mkdir", "-p", "control/figures/"])
+    sub.check_call(["rm", "-rf", "control_final_five/figures/"])
+    sub.check_call(["mkdir", "-p", "control_final_five/figures/"])
 
     plt_h_cross_section('control/')
     plt_state('control/')
+    plot_channel_transport('control/')
+
+    plt_h_cross_section('control_final_five/')
+    plt_state('control_final_five/')
+    plot_channel_transport('control_final_five/')
