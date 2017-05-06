45615485a818166985e17ba73bf24b002625d8e2
Ed Doddridge <edoddridge@gmail.com>
2017-05-06 12:00:58 -0400

    Add test with freesurfFac=1
    Now that this parameter choice works it should be tested. This is a clone of the beta_plane_gyre test, but with freesurfFac set to 1. Doing this requires an entire directory until #132 is fixed, since freesurfFac can't be altered in the call to `simulate`.

--- test/output_preservation_test.py
+++ test/output_preservation_test.py
@@ -150,3 +150,17 @@ def test_beta_plane_gyre():
                      nx=10, ny=10, exe=test_executable, dx=xlen/10, dy=ylen/10)
         assert_outputs_close(10, 10, 2, 3e-12)
         assert_volume_conservation(10, 10, 2, 1e-5)
+
+def test_beta_plane_gyre_free_surf():
+    xlen = 1e6
+    ylen = 2e6
+    nx = 10; ny = 20
+    layers = 2
+    grid = aro.Grid(nx, ny, layers, xlen / nx, ylen / ny)
+    def wind(_, Y):
+        return 0.05 * (1 - np.cos(2*np.pi * Y/np.max(grid.y)))
+    with working_directory(p.join(self_path, "beta_plane_gyre_free_surf")):
+        drv.simulate(zonalWindFile=wind, valgrind=False,
+                     nx=nx, ny=ny, exe=test_executable, dx=xlen/nx, dy=ylen/ny)
+        assert_outputs_close(nx, ny, layers, 3e-12)
+        assert_volume_conservation(nx, ny, layers, 1e-5)
