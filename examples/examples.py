e115c1deab6f748043e67a756ef1a47a4c3be334
Ed Doddridge <edoddridge@gmail.com>
2017-06-15 22:35:11 -0400

    create grid objects and plot output
    Each example now creates its own grid object and plots the output into a series of .png files.

--- examples/examples.py
+++ examples/examples.py
@@ -35,6 +35,8 @@ def beta_plane_bump_red_grav():
     ylen = 1e6
     nx = 100
     ny = 100
+    layers = 1 # also set in aronnax.conf file
+    grid = aro.Grid(nx, ny, layers, xlen / nx, ylen / ny)
 
     with working_directory(p.join(self_path,
                             "reduced_gravity/beta_plane_bump")):
@@ -42,8 +44,8 @@ def beta_plane_bump_red_grav():
                      exe=executable,
                      nx=nx, ny=ny,
                      dx=xlen/nx, dy=ylen/ny)
-        # todo: plot output
-
+        with working_directory("figures"):
+            plt_output(grid)
 
 def beta_plane_bump_n_layer():
     print "Running n-layer beta-plane bump example"
@@ -51,14 +53,16 @@ def beta_plane_bump_n_layer():
     ylen = 1e6
     nx = 100
     ny = 100
+    layers = 2 # also set in aronnax.conf file
+    grid = aro.Grid(nx, ny, layers, xlen / nx, ylen / ny)
 
     with working_directory(p.join(self_path, "n_layer/beta_plane_bump")):
         drv.simulate(initHfile=[bump, lambda X, Y: 2000. - bump(X, Y)],
                      exe=executable,
                      nx=nx, ny=ny,
                      dx=xlen/nx, dy=ylen/ny)
-        # todo: plot output
-
+        with working_directory("figures"):
+            plt_output(grid)
 
 def beta_plane_gyre_red_grav():
     print "Running reduced gravity beta-plane gyre example"
@@ -66,7 +70,8 @@ def beta_plane_gyre_red_grav():
     ylen = 2e6
     nx = 100 
     ny = 200
-    grid = aro.Grid(nx, ny, 1, xlen / nx, ylen / ny)
+    layers = 1 # also set in aronnax.conf file
+    grid = aro.Grid(nx, ny, layers, xlen / nx, ylen / ny)
 
     def wind(_, Y):
         return 0.05 * (1 - np.cos(2*np.pi * Y/np.max(Y)))
@@ -77,7 +82,8 @@ def beta_plane_gyre_red_grav():
                      nx=nx, ny=ny,
                      dx=xlen/nx, dy=ylen/ny,
                      exe=executable)
-        # todo: plot output
+        with working_directory("figures"):
+            plt_output(grid, colour_lim=20)
 
 def beta_plane_gyre_n_layer():
     print "Running n-layer beta-plane gyre example"
@@ -96,7 +102,39 @@ def beta_plane_gyre_n_layer():
                      exe=executable,
                      nx=nx, ny=ny, layers=layers,
                      dx=xlen/nx, dy=ylen/ny)
-        # todo: plot output
+        with working_directory("figures"):
+            plt_output(grid)
+
+def plt_output(grid, colour_lim=2):
+    h_files = sorted(glob.glob("../output/snap.h.*"))
+    v_files = sorted(glob.glob("../output/snap.v.*"))
+
+
+    # plot each state of the run
+
+    for i in xrange(len(v_files)):
+        h = aro.interpret_raw_file(h_files[i], grid.nx, grid.ny, grid.layers)
+        v = aro.interpret_raw_file(v_files[i], grid.nx, grid.ny, grid.layers)
+
+        X,Y = np.meshgrid(grid.x/1e3, grid.y/1e3)
+
+        plt.figure()
+        CS = plt.contour(X,Y,np.transpose(h[:,:,0]),colors='k')
+        plt.clabel(CS, inline=1, fontsize=10)
+        X,Y = np.meshgrid(grid.x/1e3, grid.yp1/1e3)
+
+        plt.pcolormesh(X,Y,np.transpose(v[:,:,0])*100., cmap='RdBu_r'
+            ,vmin = -colour_lim, vmax = colour_lim)
+        CB = plt.colorbar()
+        CB.set_label('y component of velocity (cm / s)')
+        plt.axes().set_aspect('equal')
+        plt.xlabel('x coordinate (km)')
+        plt.ylabel('y coordinate (km)')
+        plt.title('timestep {0}'.format(v_files[i][-10:]))
+
+        plt.savefig('state_{0}.png'.format(v_files[i][-10:]), dpi=150,
+            bbox_inches='tight')
+        plt.close()
 
 
 if __name__ == '__main__':
