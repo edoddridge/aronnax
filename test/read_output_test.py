630e6eff61a61083e831f8564b063fa1da951004
Ed Doddridge <ewd@mit.edu>
2018-12-21 10:05:04 -0500

    Test for fixed open_mfdataarray function

--- test/read_output_test.py
+++ test/read_output_test.py
@@ -15,10 +15,52 @@ import glob
 
 self_path = p.dirname(p.abspath(__file__))
 
-def test_open_mfdataarray():
+
+def test_open_mfdataarray_u_location():
     '''Open a number of files and assert that the length of the time
-        dimension is the same as the number of files.'''
-    
+        dimension is the same as the number of files, and that the
+        correct x and y variables have been used.'''
+
+    xlen = 1e6
+    ylen = 2e6
+    nx = 10; ny = 20
+    layers = 1
+    grid = aro.Grid(nx, ny, layers, xlen / nx, ylen / ny)
+
+    with working_directory(p.join(self_path, "beta_plane_gyre_red_grav")):
+
+        output_files = glob.glob('output/snap.u*')
+        ds = aro.open_mfdataarray(output_files, grid)
+
+        assert len(output_files) == ds.time.shape[0]
+        assert nx+1 == ds.xp1.shape[0]
+        assert ny == ds.y.shape[0]
+
+def test_open_mfdataarray_v_location():
+    '''Open a number of files and assert that the length of the time
+        dimension is the same as the number of files, and that the
+        correct x and y variables have been used.'''
+
+    xlen = 1e6
+    ylen = 2e6
+    nx = 10; ny = 20
+    layers = 1
+    grid = aro.Grid(nx, ny, layers, xlen / nx, ylen / ny)
+
+    with working_directory(p.join(self_path, "beta_plane_gyre_red_grav")):
+
+        output_files = glob.glob('output/snap.v*')
+        ds = aro.open_mfdataarray(output_files, grid)
+
+        assert len(output_files) == ds.time.shape[0]
+        assert nx == ds.x.shape[0]
+        assert ny+1 == ds.yp1.shape[0]
+
+def test_open_mfdataarray_h_location():
+    '''Open a number of files and assert that the length of the time
+        dimension is the same as the number of files, and that the
+        correct x and y variables have been used.'''
+
     xlen = 1e6
     ylen = 2e6
     nx = 10; ny = 20
@@ -30,7 +72,8 @@ def test_open_mfdataarray():
         ds = aro.open_mfdataarray(output_files, grid)
 
         assert len(output_files) == ds.time.shape[0]
-
+        assert nx == ds.x.shape[0]
+        assert ny == ds.y.shape[0]
     
 
 def test_open_mfdataarray_multiple_variables():
